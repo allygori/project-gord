63d46b686b89bcea2f093cc151137807c343a2cb
purbatua <purbatua.pasaribu@gmail.com>
2024-07-11 12:57:16 +0700

    Update category section

--- src/app/components/sections/home/category/category.tsx
+++ src/app/components/sections/home/category/category.tsx
@@ -14,7 +14,7 @@ const Category = ({ className = "" }: Props) => {
       <Container className="over flex w-full flex-row flex-wrap py-10 xl:w-10/12">
         <ul className="m-0 flex w-full flex-col justify-between p-0 xl:flex-row">
           <li className="flex flex-1 flex-col items-center justify-start p-4 text-center xl:p-8">
-            <IconCatCurtain className="mb-2 h-24 w-24 text-gray-700" />
+            <IconCatCurtain className="mb-2 h-24 w-24 text-brand-100" />
             <h3 className="mb-2 text-base font-medium text-brand-100">
               Gorden
             </h3>
@@ -25,7 +25,7 @@ const Category = ({ className = "" }: Props) => {
             </p>
           </li>
           <li className="flex flex-1 flex-col items-center justify-start p-4 text-center xl:p-8">
-            <IconCatVitrase className="mb-2 h-24 w-24 text-gray-800" />
+            <IconCatVitrase className="mb-2 h-24 w-24 text-brand-100" />
             <h3 className="mb-2 text-base font-medium text-brand-100">
               Vitrase
             </h3>
@@ -37,7 +37,7 @@ const Category = ({ className = "" }: Props) => {
             </p>
           </li>
           <li className="flex flex-1 flex-col items-center justify-start p-4 text-center xl:p-8">
-            <IconCatBlind className="mb-2 h-24 w-24 text-gray-700" />
+            <IconCatBlind className="mb-2 h-[5.72rem] w-[5.72rem] text-brand-100" />
             <h3 className="mb-2 text-base font-medium text-brand-100">Blind</h3>
             <p className="text-sm text-brand-100">
               Gorden yang terdiri dari lapisan-lapisan bilah horizontal atau
